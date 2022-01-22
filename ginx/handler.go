ebd008ffb60c5e4e7b0d34c41f5003252c28c177
luoxiongjian <2765214881@qq.com>
2022-01-22 08:31:22 +0800

    move SwaggerHandler to ginx pkg

--- ginx/handler.go
+++ ginx/handler.go
@@ -1,10 +1,32 @@
 package ginx
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
 
 type Handler interface {
 	Group() Group
 	Method() string
 	Path() string
 	Handle() gin.HandlerFunc
+}
+
+type SwaggerHandler struct{}
+
+func (h *SwaggerHandler) Group() Group {
+	return nil
+}
+
+func (h *SwaggerHandler) Method() string {
+	return MethodGet
+}
+
+func (h *SwaggerHandler) Path() string {
+	return "/swagger/*any"
+}
+
+func (h *SwaggerHandler) Handle() gin.HandlerFunc {
+	return ginSwagger.WrapHandler(swaggerFiles.Handler)
 }
